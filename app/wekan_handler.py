c3e8aa783d6480daa2afef51da6f54d06d2e1f7a
Serafim Lyowin <87949621+zxcvve@users.noreply.github.com>
2024-02-19 14:16:25 +0300

    edit create_new_card function to accept list that we need to create the card in

--- app/wekan_handler.py
+++ app/wekan_handler.py
@@ -9,15 +9,19 @@ WEKAN_BASE_URL = os.environ["WEKAN_BASE_URL"]
 WEKAN_LOGIN = os.environ["WEKAN_LOGIN"]
 WEKAN_PASSWORD = os.environ["WEKAN_PASSWORD"]
 
-client = WekanClient(base_url=WEKAN_BASE_URL,
-                     username=WEKAN_LOGIN,
-                     password=WEKAN_PASSWORD)
+client = WekanClient(
+    base_url=WEKAN_BASE_URL, username=WEKAN_LOGIN, password=WEKAN_PASSWORD
+)
 
 board: Board = client.list_boards(regex_filter="GRAD")[0]
-wekan_list: List = board.list_lists(regex_filter="BACKLOG")[0]
 swimlane: Swimlane = board.list_swimlanes()[0]
 
+backlog_list: List = board.list_lists(regex_filter="BACKLOG")[0]
+todo_list: List = board.list_lists(regex_filter="TO DO (dev.this.release)")[0]
 
-def create_new_card(description="", title="") -> str:
-    new_card: Card = wekan_list.add_card(title=title, swimlane=swimlane, description=description)
-    return (f"{client.base_url}/b/{board.id}/{board.slug}/{new_card.id}")  # url to the created card
+
+def create_new_card(list: List, description="", title="") -> str:
+    new_card: Card = list.add_card(
+        title=title, swimlane=swimlane, description=description
+    )
+    return f"{client.base_url}/b/{board.id}/{board.slug}/{new_card.id}"  # url to the created card
